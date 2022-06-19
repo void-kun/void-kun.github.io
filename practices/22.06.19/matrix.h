cd990952c80669b2628e7befde0fa1ee03b35a9b
void-kun <hoangzrik@gmail.com>
2022-06-19 22:02:49 +0700

    update

---
+++ practices/22.06.19/matrix.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+#define matrix std::vector<std::vector<double>>
+
+matrix static multiply(const matrix, const matrix);
+matrix static power(const matrix, int8_t);
