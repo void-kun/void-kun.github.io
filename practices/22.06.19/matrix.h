a40f1842e2117ae3ec0f1a4fc2729ee5b5ccd03b
void-kun <hoangzrik@gmail.com>
2022-07-13 20:31:49 +0700

    add project frontend

--- practices/22.06.19/matrix.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vector>
-
-#define matrix std::vector<std::vector<double>>
-
-matrix static multiply(const matrix, const matrix);
-matrix static power(const matrix, int8_t);
---
+++ practices/dropbox_board/src/pages/home/Home.page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar';
+import './Home.page';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default HomePage
