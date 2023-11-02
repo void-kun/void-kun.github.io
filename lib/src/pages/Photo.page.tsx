edd81c7c90f74c9bb3e1bcd487fa78021d406c90
void-kun <hoangzrik@gmail.com>
2023-11-02 20:03:33 +0700

    Remove old blog

--- lib/src/pages/Photo.page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Photo = () => {
-  return <div>Nothing :D</div>;
-};
-
-export default Photo;
--- lib/src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.scss' {
-  const content: { [key: string]: any };
-  export = content;
-}
-
-declare module '*.png' {
-  const content: any;
-  export = content;
-}
