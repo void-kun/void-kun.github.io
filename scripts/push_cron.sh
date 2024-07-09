b7d6fcb32ba034964870a78817050beb67596391
void-kun <hoangzrik@gmail.com>
2024-07-09 16:51:16 +0700

    Add cron job for auto commit

--- scripts/push_cron.sh
+++ scripts/push_cron.sh
@@ -15,16 +15,22 @@ git_branch () {
     git branch --show-current
 }
 
+
 echo "$1"
+if [[ $1 == "" ]]; then
+    commit_msg="update"
+else
+    commit_msg="$1"
+fi
 
 any_update="$(check_any_update)"
 if [[ $any_update == "Has update" ]]; then
-    local branch="$(git_branch)"
+    branch="$(git_branch)"
     echo "Commit all change to branch $branch"
 
     # add all change and push to master branch
     git add .
-    git commit -m "update"
+    git commit -m "$commit_msg"
     git push origin $branch
 else
     echo $any_update
