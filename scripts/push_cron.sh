884d05cb543b2d17ba2b37bf7161fd35236db03a
void-kun <hoangzrik@gmail.com>
2024-07-09 16:42:46 +0700

    update

--- scripts/push_cron.sh
+++ scripts/push_cron.sh
@@ -18,12 +18,12 @@ git_branch () {
 any_update="$(check_any_update)"
 if [[ $any_update == "Has update" ]]; then
     local branch="$(git_branch)"
-    echo "Commit all change to branch $branch"
+    echo "Commit all change to branch " $branch
 
     # add all change and push to master branch
     git add .
     git commit -m "update"
-    git push origin "$branch"
+    git push origin $branch
 else
     echo $any_update
 fi
