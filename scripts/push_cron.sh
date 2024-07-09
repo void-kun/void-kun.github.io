094a3c2043c6439c842f980aec66acc5dfdb7b60
void-kun <hoangzrik@gmail.com>
2024-07-09 16:34:22 +0700

    update

--- scripts/push_cron.sh
+++ scripts/push_cron.sh
@@ -3,5 +3,12 @@
 # commit all change to git
 
 check_any_update () {
-    
+    local empty_msg='nothing to commit, working tree clean'
+    local status="$(git status)"
+
+    if [[ $status == *"$empty_msg"* ]]; then
+	echo "Nothing to commit"
+    fi
 }
+
+check_any_update
