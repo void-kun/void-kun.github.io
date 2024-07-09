5efe8fc69abc36cc218e1785f4d9f9a3913f4119
void-kun <hoangzrik@gmail.com>
2024-07-09 16:40:24 +0700

    update

--- scripts/push_cron.sh
+++ scripts/push_cron.sh
@@ -9,6 +9,21 @@ check_any_update () {
     if [[ $status == *"$empty_msg"* ]]; then
 	echo "Nothing to commit"
     fi
+
+    echo "Has update"
 }
 
-check_any_update
+git_branch () {
+    git rev-parse --abbrev-ref HEAD
+}
+
+any_update="$(check_any_update)"
+if [[ $any_update == "Has update" ]]; then
+    echo $any_update
+    # add all change and push to master branch
+    git add .
+    git commit -m "update"
+    git push origin "$(git_branch)"
+else
+    echo $any_update
+fi
